a92bf76ca8e33795ccb4b53734f86ecf8606329d
Edvard <edvardp87>
2015-08-17 23:50:47 -0300

    teste

---
+++ Teste/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Teste.Startup))]
+
+namespace Teste
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
